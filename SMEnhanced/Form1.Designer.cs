e326858da9a0ddb872bfce3b8da66107dc5dd0a7
Overial <dfjx2201@yandex.ru>
2021-11-09 18:36:35 +0300

    Initial commit

---
+++ SMEnhanced/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace SMEnhanced
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matrix dimension NxN =";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 242);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(242, 164);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Analyze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(304, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(417, 417);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Current arrangement:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 460);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Arrangement options amount: 0";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(301, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 22);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Board NxN:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(50, 63);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 21);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "BFS";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(50, 90);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(110, 21);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Backtracking";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Select searching method:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 412);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Previous";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(129, 412);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 29);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Next";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Results:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 194);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Steps amount: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 491);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "N Queens Puzzle";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SMEnhanced/Form1.cs
@@ -0,0 +1,552 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMEnhanced
+{
+    public partial class Form1 : Form
+    {
+        int dimension;
+
+        int bfsMethodStepsCount;
+        int btMethodStepsCount;
+
+        // Returs true if xCoords are en prise
+        bool AreTwoQueensEnPrise(int x1, int y1, int x2, int y2)
+        {
+            // Check for equal coords
+            if (x1 == x2 && y1 == y2)
+            {
+                return true;
+            }
+
+            // We need to cast rays in needed directions
+            int i = x1;
+            int j = y1;
+
+            // NW
+            while (i >= 1 && j >= 1)
+            {
+                // Found queen
+                if (i == x2 && j == y2)
+                {
+                    return true;
+                }
+
+                --i;
+                --j;
+            }
+
+            // N
+            i = x1;
+            j = y1;
+            while (i >= 1)
+            {
+                // Found queen
+                if (i == x2 && j == y2)
+                {
+                    return true;
+                }
+
+                --i;
+            }
+
+            // NE
+            i = x1;
+            j = y1;
+            while (i >= 1 && j <= dimension)
+            {
+                // Found queen
+                if (i == x2 && j == y2)
+                {
+                    return true;
+                }
+
+                --i;
+                ++j;
+            }
+
+            // W
+            i = x1;
+            j = y1;
+            while (j >= 1)
+            {
+                // Found queen
+                if (i == x2 && j == y2)
+                {
+                    return true;
+                }
+
+                --j;
+            }
+
+            // E
+            i = x1;
+            j = y1;
+            while (j <= dimension)
+            {
+                // Found queen
+                if (i == x2 && j == y2)
+                {
+                    return true;
+                }
+
+                ++j;
+            }
+
+            // SW
+            i = x1;
+            j = y1;
+            while (i <= dimension && j >= 1)
+            {
+                // Found queen
+                if (i == x2 && j == y2)
+                {
+                    return true;
+                }
+
+                ++i;
+                --j;
+            }
+
+            // S
+            i = x1;
+            j = y1;
+            while (i <= dimension)
+            {
+                // Found queen
+                if (i == x2 && j == y2)
+                {
+                    return true;
+                }
+
+                ++i;
+            }
+
+            // SE
+            i = x1;
+            j = y1;
+            while (i <= dimension && j <= dimension)
+            {
+                // Found queen
+                if (i == x2 && j == y2)
+                {
+                    return true;
+                }
+
+                ++i;
+                ++j;
+            }
+
+            return false;
+        }
+
+        // Returns true if current queen is en prise, false if is not en prise
+        bool IsQueenEnPrise(int[] xCoords, int iCurrentQueenYCoord)
+        {
+            // Check every previous queen before current
+            for (int j = 1; j <= iCurrentQueenYCoord - 1; ++j)
+            {
+                if (AreTwoQueensEnPrise(xCoords[j], j, xCoords[iCurrentQueenYCoord], iCurrentQueenYCoord))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Method to draw board on screen
+        void DrawBoard()
+        {
+            // Disable user board management
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+
+            // Clear board
+            this.dataGridView1.Columns.Clear();
+
+            // Center alignment
+            this.dataGridView1.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            // Set column height
+            this.dataGridView1.RowTemplate.Height = 32;
+
+            // Set columns
+            for (int i = 0; i < dimension; ++i)
+            {
+                // DataGridViewImageColumn imageCol = new DataGridViewImageColumn();
+                // this.dataGridView1.Columns.Add(imageCol);
+
+                // Initialize board column
+                this.dataGridView1.Columns.Add(i.ToString(), (i + 1).ToString());
+
+                // Set column width
+                this.dataGridView1.Columns[i].Width = 32;
+            }
+
+            // Set rows
+            this.dataGridView1.Rows.Add(dimension);
+
+            // Print column numbers
+            for (int i = 0; i < dimension; ++i)
+            {
+                this.dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+            }
+        }
+
+        // Method to create arrangement option
+        void AddNewOptionToList(int[] row)
+        {
+            // Set arrangement option variable
+            string option = "";
+
+            // Fill arrangement option
+            if (radioButton1.Checked)
+            {
+                for (int i = 1; i <= dimension; ++i)
+                {
+                    option += row[i].ToString() + "," + i.ToString() + ";";
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                for (int i = 0; i < dimension; ++i)
+                {
+                    option += row[i].ToString() + "," + i.ToString() + ";";
+                }
+            }
+
+            // Add arrangement option to list
+            this.listBox1.Items.Add(option);
+        }
+
+        // Method to draw xCoords on board
+        void DrawQueensOnBoard(string option)
+        {
+            // Clear board
+            for (int i = 0; i < dimension; ++i)
+            {
+                for (int j = 0; j < dimension; ++j)
+                {
+                    // this.dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.White;
+
+                    this.dataGridView1.Rows[i].Cells[j].Value = "";
+                }
+            }
+
+            // Display column with xCoords
+            int k = 0;
+            for (int i = 0; i < dimension; ++i)
+            {
+                // Parse x coord
+                string xOption = "";
+                while (option[k] != ',')
+                {
+                    xOption += option[k].ToString();
+                    ++k;
+                }
+                ++k;
+
+                // Parse y coord
+                string yOption = "";
+                while (option[k] != ';')
+                {
+                    yOption += option[k].ToString();
+                    ++k;
+                }
+                ++k;
+
+                // Set queen coord
+                int xQueen = Int32.Parse(xOption);
+                int yQueen = Int32.Parse(yOption);
+
+                // Bitmap img = new Bitmap(@"D:\queen_image.png");
+                // this.dataGridView1.Rows[yQueen].Cells[xQueen].Value = img;
+                // this.dataGridView1.Rows[yQueen].Cells[xQueen].Value = Image.FromFile(@"D:\queen_image.png");
+                // this.dataGridView1.Rows[yQueen].Cells[xQueen].Style.BackColor = Color.Black;
+
+                // Display queen on board
+                if (radioButton1.Checked)
+                {
+                    this.dataGridView1.Rows[yQueen - 1].Cells[xQueen - 1].Value = "Q";
+                }
+                else if (radioButton2.Checked)
+                {
+                    this.dataGridView1.Rows[yQueen].Cells[xQueen].Value = "Q";
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Check BFS option
+            this.radioButton1.Checked = true;
+        }
+
+        // Show selected option
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            // If list is empty then do nothing
+            if (this.listBox1.Items.Count <= 0)
+            {
+                return;
+            }
+
+            // Get selected option
+            int iSelectedOption = this.listBox1.SelectedIndex;
+
+            // Draw current option on board
+            DrawQueensOnBoard(this.listBox1.Items[iSelectedOption].ToString());
+        }
+
+        private void listBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // If list is empty then do nothing
+            if (this.listBox1.Items.Count <= 0)
+            {
+                return;
+            }
+
+            // Get selected option
+            int iSelectedOption = this.listBox1.SelectedIndex;
+
+            // Draw current option on board
+            DrawQueensOnBoard(this.listBox1.Items[iSelectedOption].ToString());
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // If list is empty then do nothing
+            if (this.listBox1.Items.Count <= 0)
+            {
+                return;
+            }
+
+            // Get selected option
+            int iSelectedOption = this.listBox1.SelectedIndex;
+
+            // Draw current option on board
+            DrawQueensOnBoard(this.listBox1.Items[iSelectedOption].ToString());
+        }
+
+        public void BFSSearch(ref int iOptionsCount)
+        {
+            // xCoords[iCurrentQueenYCoord] – X
+            // iCurrentQueenYCoord – Y
+
+            // Queen X coords
+            int[] xCoords = new int[dimension + 1];
+
+            // Current queen Y coord
+            int iCurrentQueenYCoord = 1;
+
+            // Initial queen X coords
+            xCoords[0] = 0;
+            xCoords[iCurrentQueenYCoord] = 0;
+
+            // Set every queen
+            while (iCurrentQueenYCoord > 0)
+            {
+                ++bfsMethodStepsCount;
+                xCoords[iCurrentQueenYCoord] += 1;
+
+                // The last element
+                if (iCurrentQueenYCoord == dimension)
+                {
+                    // Rollback
+                    if (xCoords[iCurrentQueenYCoord] > dimension)
+                    {
+                        while (xCoords[iCurrentQueenYCoord] > dimension)
+                        {
+                            --iCurrentQueenYCoord;
+                        }
+                    }
+                    else if (!IsQueenEnPrise(xCoords, iCurrentQueenYCoord))
+                    {
+                        // Set new option
+                        AddNewOptionToList(xCoords);
+                        ++iOptionsCount;
+                        --iCurrentQueenYCoord;
+                    }
+                }
+                // Not the last element
+                else
+                {
+                    // Rollback
+                    if (xCoords[iCurrentQueenYCoord] > dimension)
+                    {
+                        while (xCoords[iCurrentQueenYCoord] > dimension)
+                        {
+                            --iCurrentQueenYCoord;
+                        }
+                    }
+                    // Move to next column
+                    else if (!IsQueenEnPrise(xCoords, iCurrentQueenYCoord))
+                    {
+                        ++iCurrentQueenYCoord;
+                        xCoords[iCurrentQueenYCoord] = 0;
+                    }
+                }
+            }
+        }
+
+        // Board for BT method
+        int[] board;
+
+        // Check current tile
+        public bool IsTileSafe(int row, int col)
+        {
+            int j = 0;
+            while (j < col)
+            {
+                if (// Check row
+                    row == board[j] ||
+                    // Check left diagonal
+                    col - row == j - board[j] ||
+                    // Check right diagonal
+                    col + row == j + board[j])
+                {
+                    return false;
+                }
+
+                ++j;
+            }
+
+            return true;
+        }
+
+        // Recursive backtracking method
+        public void BacktrackingMethod(int col, ref int iOptionsCount)
+        {
+            ++btMethodStepsCount;
+
+            if (col == dimension)
+            {
+                ++iOptionsCount;
+
+                AddNewOptionToList(board);
+            }
+
+            for (int row = 0; row < dimension; ++row)
+            {
+                if (IsTileSafe(row, col))
+                {
+                    board[col] = row;
+                    BacktrackingMethod(col + 1, ref iOptionsCount);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+
+            // Prevent invalid input
+            try
+            {
+                // Get matrix dimension
+                this.dimension = Convert.ToInt32(this.textBox1.Text);
+
+                // 4 < dimension < 8
+                if (dimension < 4)
+                {
+                    dimension = 4;
+                }
+                else if (dimension > 8)
+                {
+                    dimension = 8;
+                }
+
+                // Display board dimension
+                label4.Text = "Board " + dimension + "x" + dimension + ":";
+            }
+            catch
+            {
+                this.textBox1.Clear();
+            }
+
+            this.textBox1.Clear();
+            DrawBoard();
+
+            if (this.radioButton1.Checked)
+            {
+                // Amount of options
+                int iOptionsCount = 0;
+
+                // Call BFS method function
+                BFSSearch(ref iOptionsCount);
+
+                if (iOptionsCount > 0)
+                {
+                    // Display options arrangement options amount
+                    this.listBox1.SelectedIndex = 0;
+                    this.listBox1_Click(sender, e);
+                    label2.Text = "Arrangement options amount: " + iOptionsCount.ToString();
+
+                    // Display steps amount to capture effectiveness
+                    label7.Text = "Steps amount: " + bfsMethodStepsCount.ToString();
+                    bfsMethodStepsCount = 0;
+                }
+                else
+                {
+                    label2.Text = "Arrangement options amount: 0";
+                }
+            }
+            else if (this.radioButton2.Checked)
+            {
+                int iOptionsCount = 0;
+
+                board = new int[dimension];
+
+                // Call backtracking method function
+                BacktrackingMethod(0, ref iOptionsCount);
+
+                if (iOptionsCount > 0)
+                {
+                    // Display options arrangement options amount
+                    this.listBox1.SelectedIndex = 0;
+                    this.listBox1_Click(sender, e);
+                    label2.Text = "Arrangement options amount: " + iOptionsCount.ToString();
+
+                    // Display steps amount to capture effectiveness
+                    label7.Text = "Steps amount: " + btMethodStepsCount.ToString();
+                    btMethodStepsCount = 0;
+                }
+                else
+                {
+                    label2.Text = "Arrangement options amount: 0";
+                }
+            }
+        }
+
+        // Previous listBox choice
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.listBox1.SelectedIndex > 0)
+            {
+                --this.listBox1.SelectedIndex;
+            }
+        }
+
+        // Next listBox choice
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.listBox1.SelectedIndex < this.listBox1.Items.Count - 1)
+            {
+                ++this.listBox1.SelectedIndex;
+            }
+        }
+    }
+}
